5e1f633c6f076b982a55e02edddaf5c7f0a9bdd7
edgarosf <edgaromarsf@gmail.com>
2024-05-27 00:30:46 -0600

    first commit

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react"
+
+import Card from "../../Components/Card"
+import Layout from "../../Components/Layout"
+
+function Home() {
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+            .then(response => response.json())
+            .then(data => setItems(data))
+            .catch(err => console.error(err))
+  }, [])
+
+    return (
+      <Layout>
+      home
+      <div className="grid gap-2 grid-cols-4 w-full max-w-screen-lg">
+        {
+          items?.map((item) => <Card key={item.id} data={item} />)
+        }  
+      </div>
+      </Layout>
+    )
+  }
+  
+  export default Home
